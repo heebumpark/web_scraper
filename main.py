e138ada00591ebb8502821bd0ada7563301bbd09
maplewilliam <maplewilliam@naver.com>
2022-04-27 18:07:31 +0900

    1차 완료

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ jobs = website_soup.find("div",{"id": "MainSuperBrand"}).find("ul",{"class": "go
 for job in jobs:
   URL = job.find("a",{"class":"goodsBox-info"})['href']
   name = job.find("span",{"class":"company"}).text
-  place = []
-  title = []
-  time = []
-  pay = []
-  date = []
+
   company_website = requests.get(URL)
   company_soup = BeautifulSoup(company_website.text, "html.parser")
 
   informations = company_soup.find("div", {"id" : "NormalInfo"}).find("table").find("tbody").find_all("tr")
+  file = open(f'{name}.csv', mode="w", encoding='UTF-8')
+  writer = csv.writer(file)
 
+  writer.writerow(["place", "title", "time", "pay", "date"])
+  
   for information in informations:
     
     info_place = information.find("td",{"class":"local"})
@@ -32,23 +32,15 @@ for job in jobs:
     info_pay = information.find("td",{"class": "pay"})
     info_date = information.find("td",{"class":"regDate"})
     
-
     if info_place == None:
       continue
+    data = []
 
-    place.append(info_place.text)
-    title.append(info_title.text)
-    time.append(info_time.text)
-    pay.append(info_pay.text)
-    date.append(info_date.text)
-   
+    data.append(info_place.text)
+    data.append(info_title.text)
+    data.append(info_time.text)
+    data.append(info_pay.text)
+    data.append(info_date.text)
+    writer.writerow(data)
 
-    #print(f"장소 : {info_place.text}, 제목 : {info_title.text}, 시간 : {info_time}, 돈 : {info_pay.text}, 몇분전 : {info_date.text} ")
-  print()
-  print()
-  print()
-  print()
-  print()
-  print(f"----------{name}----------")
-  for i in range(len(place)):
-    print(f"{place[i]},     {title[i]},     {time[i]},      {pay[i]},    {date[i]} ")
+
--- prac.py
+++ prac.py
@@ -1,2 +1,5 @@
-lst = ['naver.com    ','   yahoo.com']
-lst
+lst = []
+v1 = 'hh'
+v2 = 'hekqw'
+lst.append(v1+v2)
+print(lst)
